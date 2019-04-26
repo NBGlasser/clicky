1f70229f5112a23dcd7bbf385d1dcafafea4903b
Noah G <NoahBGlasser@gmail.com>
2019-04-25 19:33:02 -0700

    got setstate working

--- clicky/src/components/pages/ClickContainer.js
+++ clicky/src/components/pages/ClickContainer.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
-import Images from "/../../images"
+let array = []
+for (let i = 0; i < 13; i++) {
+    array.push(`/assets/images/${i}.jpg`)
+}
 
 class ClickContainer extends Component {
 
     state = {
-        images: [],
-        one: "unclicked",
-        two: "unclicked",
-        three: "unclicked",
-        four: "unclicked",
-        five: "unclicked",
-        six: "unclicked",
-        seven: "unclicked",
-        eight: "unclicked",
-        nine: "unclicked",
-        ten: "unclicked",
-        eleven: "unclicked",
-        twelve: "unclicked",
+        images: array,
+        "1": "unclicked",
+        "2": "unclicked",
+        "3": "unclicked",
+        "4": "unclicked",
+        "5": "unclicked",
+        "6": "unclicked",
+        "7": "unclicked",
+        "8": "unclicked",
+        "9": "unclicked",
+        "10": "unclicked",
+        "11": "unclicked",
+        "12": "unclicked",
         score: 0
     }
 
     componentDidMount() {
-        this.load()
+        this.shuffle()
     }
 
-    load() {
-        var arr = []
-        for (let i = 0; i < Images.length; i++) {
-            arr.push(Images[i])
+    shuffle() {
+        let array = []
+        for (let i = 0; i < 13; i++) {
+            array.push(`/assets/images/${i}.jpg`)
         }
-        function shuffle(array) {
+         const shuffle = (array) => {
             var currentIndex = array.length, temporaryValue, randomIndex;
 
             while (0 !== currentIndex) {
@@ -44,33 +47,35 @@ class ClickContainer extends Component {
 
             return array;
         }
-        
-        this.state.images = shuffle(arr)
+        array = shuffle(array)
+        this.shuffle = this.shuffle.bind(this)
+        this.setState({ images: array })
+
     }
 
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <img src={this.state.images[1]} className="col-4" /> <img src={this.state.images[2]} className="col-4" /> <img src={this.state.images[3]} className="col-4" />
+                    <img src={this.state.images[1]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[2]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[3]} className="col-4" onClick={this.shuffle} />
                 </div>
 
 
                 <div className="container">
                     <div className="row">
-                        <img src={this.state.images[4]} className="col-4" /> <img src={this.state.images[5]} className="col-4" /> <img src={this.state.images[6]} className="col-4" />
+                        <img src={this.state.images[4]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[5]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[6]} className="col-4" onClick={this.shuffle} />
                     </div>
                 </div>
 
                 <div className="container">
                     <div className="row">
-                        <img src={this.state.images[7]} className="col-4" /> <img src={this.state.images[8]} className="col-4" /> <img src={this.state.images[9]} className="col-4" />
+                        <img src={this.state.images[7]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[8]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[9]} className="col-4" onClick={this.shuffle} />
                     </div>
                 </div>
 
                 <div className="container">
                     <div className="row">
-                        <img src={this.state.images[10]} className="col-4" /> <img src={this.state.images[11]} className="col-4" /> <img src={this.state.images[12]} className="col-4" />
+                        <img src={this.state.images[10]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[11]} className="col-4" onClick={this.shuffle} /> <img src={this.state.images[12]} className="col-4" onClick={this.shuffle} />
                     </div>
                 </div>
             </div>
